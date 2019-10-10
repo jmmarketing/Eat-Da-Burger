8045d66512bf4682a132ef6c03ead720ff1effff
JeffM <jeff@jeffreymclean.com>
2019-10-09 17:00:38 -0700

    Added schema and seeds. Created connection to sql database

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT,
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ("Double Double", false), 
+("Animal Style Burger", false),
+("Flying Dutchman", false); 
